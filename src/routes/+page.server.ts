b3d9d20e42094b64c9b4ee3dfc2a1320d00fa07e
Nicolas10Montex <montexdev@protonmail.com>
2025-04-09 20:39:45 +0200

    changed prettier

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,12 @@
 import { db } from "$lib/server/db";
 import { poll } from "$lib/server/db/schema";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async() => {
+	return {
+		serverMessage: "Hello I'm server"
+	}
+}
 
 export const actions = {
 	createPoll: async({request}) => {
